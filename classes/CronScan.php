af61f1c5b0823ae5b200a7e99a53808fc77855fd
Andy Martin <andy123@stanford.edu>
2021-12-30 13:31:04 -0800

    trying to make a more efficient scanner for EMAs that need action

--- classes/CronScan.php
+++ classes/CronScan.php
@@ -5,179 +5,188 @@ use REDCap;
 use Exception;
 
 require_once "classes/RepeatingForms.php";
+require_once APP_PATH_DOCROOT . "/Libraries/Twilio/Services/Twilio.php";
 
 /**
- * ScheduleInstance
+ * CronScan
  *
- * @property \Stanford\EMA\EMA $module
+ * A Class and functions to facilitate a cron-job scan of potential outgoing notifications
+ * It is assumed that this class is being executed in the context of the project_id
+ * where the EMA is configured and being run
+ *
+ * @property EMA $module
  */
-class ScheduleInstance
+class CronScan
 {
     private $module;
 
-    // Window parameters
-    private $window_name, $start_date, $form, $form_event_id;
-    private $window_days = array();
+    private $all_events;
 
-    // Record parameters
-    private $pid, $record_id;
+    public $windows;
+    public $schedules;
 
-    // Schedule parameters
-    private $randomize, $start_time;
-    private $sched_offsets = array();
+    private $loaded_record_id;
+    private $record_data;
 
-    // Text messages
-    private $text_msgs = array();
 
-    public const MINUTES_IN_DAY = 1440;
+    private $window;    // Current Window
 
     public function __construct($module) {
         $this->module = $module;
+
+        // Retrieve all window and schedule configs
+        [$this->windows, $this->schedules] = $this->module->getConfigAsArrays();
+
+        // Load events
+        $this->all_events = REDCap::getEventNames(true, true);
     }
 
     /**
-     * Passes configuration data from the window and schedule configs so the schedules can be created.
-     *
-     * @param $record_id
-     * @param $window_config
-     * @param $start_date
-     * @param $start_time
-     * @param $form_event_id
-     * @param $schedule_config
-     */
-    public function setUpSchedule($record_id, $window_config, $start_date, $start_time,
-                                    $form_event_id, $schedule_config)
-    {
-        // These are window parameters
-        $this->window_name              = $window_config['window-name'];
-        $this->start_date               = $start_date;
-        $this->start_time               = $start_time;
-        $this->form                     = $window_config['window-form'];
-        $this->form_event_id            = $form_event_id;
-        $this->window_days              = $window_config['window-days'];
-
-        // This is the record we are calculating the window for
-        $this->record_id                = $record_id;
-        $this->pid                      = $this->module->getProjectId();
-
-        // These are the text messages that will be sent
-        $this->text_msgs[EMA::NOTIFICATION_SENT] = $window_config['text-message'];
-        $this->text_msgs[EMA::REMINDER_1_SENT] = $window_config['text-reminder1-message'];
-        $this->text_msgs[EMA::REMINDER_2_SENT] = $window_config['text-reminder2-message'];
-
-        // This is the schedule configuration to determine days/times of surveys
-        $this->sched_offsets            = $schedule_config['schedule-offsets'];
-        $this->randomize                = $schedule_config['schedule-randomize-window'];
-    }
-
-    /**
-     * This function loops over each window day
-     */
-    public function createWindowSchedule()
-    {
-
-        // Loop over each window day and create entries for each schedule instance
-        foreach ($this->window_days as $wind_day_num) {
-
-            $new_date = $this->addDaysToDate($this->start_date, $wind_day_num);
-            $this->calculateSchedule($wind_day_num, $new_date);
-        }
-
-    }
-
-    /**
-     * This function creates each instance of the schedule for the day
-     *
-     * @param $window_num
-     * @param $start_date
      * @throws Exception
      */
-    private function calculateSchedule($window_num, $start_date) {
+    public function scanWindows() {
 
-        // Instantiate the repeating form helper class
-        try {
-            $rf = new RepeatingForms($this->pid, $this->form);
-            $next_instance_id = $rf->getNextInstanceId($this->record_id, $this->form_event_id);
-        } catch (Exception $ex) {
-            $this->module->emError("Exception when instantiating RepeatingForms class");
-            return;
-        }
+        foreach ($this->windows as $window) {
+            $window_name = $window['window-name'];
+
+            // Find the schedule configuration for this window configuration
+            $schedule_name = $window['window-schedule-name'];
+            $schedule = $this->module->findScheduleForThisWindow($schedule_name, $this->schedules);
+            if (empty($schedule)) {
+                throw new Exception ("Unable to load schedule $schedule_name for window $window_name");
+            }
+            $this->module->emDebug("Looking over window $window_name and schedule $schedule_name");
+
+            // Goal here is to find all records for this window that need to be processed
+            // A record that should be processed is one where the ema_status is less than 90:
+            // and the ema_open_ts is in the past....
+            $window_form = $window['window-form'];
+            [$window_form_event_name, $window_form_event_id] = $this->getEventNameAndId($window['window-form-event']);
+            $instances = $this->getActiveInstances($window_form_event_id);
+            if (empty($instances)) continue;
+
+            $window_opt_out_field = $window['window-opt-out-field'];
+            [$window_opt_out_event_name, $window_opt_out_event_id] = $this->getEventNameAndId($window['window-opt-out-event']);
+
+            // Instantiate a repeatingForm helper
+            $RF = new RepeatingForms($window_form, $window_form_event_id);
+
+            // Loop through each record/instance and process it
+            foreach ($instances as $i) {
+                $record_id    = $i['record_id'];
+                $instance_id  = $i['instance'];
+                $age_in_min   = $i['age_in_min'];
 
 
-        // Loop over each offset and create an instance for each offset
-        $ncounter = 1;
-        foreach($this->sched_offsets as $offset) {
+                // Load current record data once and cache it to reduce db queries
+                $this->loadRecordData($record_id);
 
-            // If randomization is being used, create a randomized offset to add to the base
-            $rand_time = (empty($this->randomize) ? 0 : $this->generateRandomOffset(0, $this->randomize));
+                // Determine if this record has opted out for this window
+                $opt_out = $this->getDataValue($record_id, $window_opt_out_event_id, $window_opt_out_field) == 1;
 
-            // Save this data on each survey.  These fields will be hidden so participants will not see them.
-            $saveSched = array();
-            $saveSched['ema_window_name']   = $this->window_name;
-            $saveSched['ema_window_day']    = $window_num;
-            $saveSched['ema_sequence']      = $ncounter++;
-            $saveSched['ema_offset']        = $offset;
-            $saveSched['ema_open']          = $this->start_time + $rand_time + $offset;
-            $saveSched['ema_open_ts']       = $this->addMinutesToDate($start_date, $saveSched['ema_open']);
-            $saveSched['ema_status']        = EMA::SCHEDULE_CALCULATED;
+                if ($opt_out) {
+                    $this->module->emDebug("Record $record_id, Event $window_opt_out_event_id, Field $window_opt_out_field for window $window_name is opted out");
+                }
 
-            // Save this info on the instrument specified
-            $instance_id = $next_instance_id++;
-            $status = $rf->saveInstance($this->record_id, $saveSched, $instance_id, $this->form_event_id);
-            if (!$status) {
-                $message = $rf->last_error_message();
-                $this->emError("Error when saving data for window $this->window_name, record $this->record_id with message: " . $message);
+                // TODO: Decide what to do for instance_id = null which is what the first instance id is for some stupid reason.
+                // if (empty($instance_id)) {
+                //     $this->module->emError("Found an empty instance id for record $record_id - this shouldn't happen!");
+                //     continue;
+                // }
+
+                if ($instance = $RF->getInstanceById($record_id,$instance_id, $window_form_event_id)) {
+
+                }
+
+
             }
         }
-
     }
 
 
-    /**
-     * Generate a random offset in minutes between min and max using a resolution specified
-     *
-     * @param $min
-     * @param $max
-     * @param $resolution   // Minimum resultion in minutes for the offset
-     * @return int
-     * @throws \Exception
-     */
-    private function generateRandomOffset($min, $max, $resolution = 1): int
-    {
-        // If max is 120 and resolution is 5, then we want a number between 0 and 24.
-        $randMax = floor($max / $resolution);
-        $randMin = $min;
-        $randInt = random_int($randMin, $randMax);
-
-        return $randInt * $resolution;
+    private function getDataValue($record_id, $event_id, $field_name) {
+        $this->loadRecordData($record_id);
+        if (isset($this->record_data[$record_id][$event_id][$field_name])) {
+            return $this->record_data[$record_id][$event_id][$field_name];
+        } else {
+            $this->module->emDebug("Unable to find record $record_id, event $event_id, field_name $field_name in cached record data");
+            return null;
+        }
     }
 
     /**
-     * This function is an utility which will add minutes to a date and returns a timestamp.
-     *
-     * @param $date
-     * @param $minutes
-     * @return string
+     * Load ALL record data from the specified events (optional)
+     * @param $record_id
+     * @param $event_ids
+     * @return void
      */
-    private function addMinutesToDate($date, $minutes) {
-
-        $datetime_in_sec = strtotime($date);
-        $new_datetime = $datetime_in_sec + $minutes*60;
-        return  date("Y-m-d H:i:s", $new_datetime);
+    private function loadRecordData($record_id, $event_ids = []) {
+        if ($this->loaded_record_id !== $record_id) {
+            $params = [];
+            $params['records'] = $record_id;
+            if (!empty($event_ids)) $params['events'] = $event_ids;
+            $this->record_data = REDCap::getData($params);
+        }
     }
 
     /**
-     * This function adds the number of days to a date and returns the new date.
-     *
-     * @param $date
-     * @param $num_days
-     * @return string
+     * Obtain all records / instances of EMA for specified event that should be further processed
+     * @param $event_id
+     * @return array
      */
-    private function addDaysToDate($date, $num_days) {
-
-        $datetime_in_sec = strtotime($date);
-        $new_date = $datetime_in_sec + $num_days*24*60*60;
-        return date("Y-m-d", $new_date);
+    private function getActiveInstances($event_id) {
+        // Query the database to obtain all instances that require processing
+        $q = $this->module->query(
+            "select
+                r1.record as record_id,
+                r1.event_id,
+                r1.instance,
+                r1.value as ema_open_ts,
+                r2.value as ema_status
+            from
+                redcap_data r1
+                join redcap_data r2 on r1.project_id = r2.project_id and r1.record = r2.record
+                                   and r1.event_id = r2.event_id and r1.instance <=> r2.instance
+            where
+                r1.project_id = ?
+            and r1.event_id = ?
+            and r1.field_name = 'ema_open_ts'
+            and current_timestamp >= str_to_date(r1.value, '%Y-%m-%d %H:%i:%s')
+            and r2.field_name = 'ema_status'
+            and cast(r2.value as unsigned) < 90", [ $this->module->getProjectId(), $event_id ]
+        );
+        $results = [];
+        while ($row = db_fetch_assoc($q)) $results[] = $row;
+        return $results;
     }
+
+
+    /** This function will accept an event name or event id and return both name and event.
+     *
+     * @param $event
+     * @return array
+     */
+    private function getEventNameAndId($event) {
+        global $Proj;
+
+        // This is a longitudinal project
+        $event_ids = array_keys($this->all_events);
+
+        if (!in_array($event, $event_ids)) {
+            // Incoming event is an event name
+            $event_name = $event;
+            $names_to_ids = array_flip($this->all_events);
+            $event_id = $names_to_ids[$event];
+        } else {
+            // Incoming event is an event id
+            $event_id = $event;
+            $event_name = $this->all_events[$event];
+        }
+        return [$event_name, $event_id];
+    }
+
+
+
+
 
 }
